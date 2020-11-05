9cf8811308e48b21d221f26483a83798ae836743
JacksonReynolds <jwreynod@gmail.com>
2020-11-05 11:43:03 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,19 +7,38 @@ class LoginForm extends React.Component {
     this.state = {};
   }
 
+  handleUsernameChange = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.password && this.state.username) {
+      this.props.handleLogin(this.state)
+    }
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleUsernameChange} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handlePasswordChange} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 
-class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+class TwitterMessage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      maxChars: this.props.maxChars,
+      tweetLength: 0
+    };
+  }
 
-    this.state = {};
+  handleTweetChange = (e) => {
+    this.setState({
+      input: e.target.value,
+      tweetLength: e.target.value.length
+    })
+  }
+  
+  charsLeft() {
+    return this.state.maxChars - this.state.tweetLength
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleTweetChange} value={this.state.input} type="text" name="message" id="message" />
+        <p>{this.charsLeft()} characters remaining</p>
       </div>
     );
   }
